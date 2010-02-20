bbf3fb5e4fef43f8385b6e2e7471b16e08a1d5a5
Tracey Ambrose <traceyambrose@aoede.local>
2010-02-20 21:36:54 +1300

    changed root path

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ set :repository,  "git@github.com:traceyambrose/foodcoop.git"
 set :deploy_to, "/home/ttnz/sites/#{application}"
 
 set :scm, :git
+set :use_sudo, false
 
 set :single_server, "foodcoop.tt.craigambrose.com"
 
@@ -10,16 +11,17 @@ role :web, single_server                          # Your HTTP server, Apache/etc
 role :app, single_server                          # This may be the same as your `Web` server
 role :db,  single_server
 
-set :username, 'ttnz'
+set :user, 'ttnz'
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart do ; end
+  task :finalize_update do ; end
+end
+
+after :deploy, "deploy:cleanup"
