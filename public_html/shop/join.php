122ac7a7f3a40e2eeb1ee94566f713f36de58c56
Tracey Ambrose <traceyambrose@aoede.local>
2010-02-20 22:35:06 +1300

    Created basic join page for membership

---
+++ public_html/shop/join.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html>
+	<head>
+		<title>Become a member</title>
+	</head>
+	<body>
+		<a href="members/member_form.php">Membership form</a>
+	</body>
+</html>
